83b069ca77d6f93295f31bf1531134f377dff610
KimDongki <kdkdongki1997@gmail.com>
2021-07-06 17:30:01 +0900

    [docs] API Docs Update

--- spring-mybatis/src/main/java/com/kdk/controller/MainRestController.java
+++ spring-mybatis/src/main/java/com/kdk/controller/MainRestController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kdk.domain.ResultVo;
 import com.kdk.service.DefaultService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 
 @Api(value = "MainRestController")
@@ -26,12 +28,20 @@ public class MainRestController {
   private DefaultService service;
 
   @ApiOperation(value = "MyBatis 입력 API", tags = "MyBatis")
+  @ApiImplicitParams({
+    @ApiImplicitParam
+    (name = "result", value = "테스트 데이터", required = true),
+  })
   @RequestMapping(value = "/insert", method = RequestMethod.POST)
   public ResponseEntity<ResultVo> insert(@RequestBody HashMap<String, Object> request) {
     ResultVo result = ResultVo.builder().resultData(service.defaultInsert(request)).build();
     return new ResponseEntity<>(result, HttpStatus.OK);
   }
 
+  @ApiImplicitParams({
+    @ApiImplicitParam
+    (name = "idx", value = "조회 Index", required = true),
+  })
   @ApiOperation(value = "MyBatis 조회 API", tags = "MyBatis")
   @RequestMapping(value = "/select", method = RequestMethod.GET)
   public ResponseEntity<ResultVo> select(String idx) {
@@ -39,6 +49,12 @@ public class MainRestController {
     return new ResponseEntity<>(result, HttpStatus.OK);
   }
 
+  @ApiImplicitParams({
+    @ApiImplicitParam
+    (name = "idx", value = "조회 Index", required = true),
+    @ApiImplicitParam
+    (name = "result", value = "테스트 데이터", required = true),
+  })
   @ApiOperation(value = "MyBatis 수정 API", tags = "MyBatis")
   @RequestMapping(value = "/update", method = RequestMethod.PUT)
   public ResponseEntity<ResultVo> update(@RequestBody HashMap<String, Object> request) {
@@ -47,6 +63,10 @@ public class MainRestController {
   }
 
   @ApiOperation(value = "MyBatis 삭제 API", tags = "MyBatis")
+  @ApiImplicitParams({
+    @ApiImplicitParam
+    (name = "idx", value = "조회 Index", required = true),
+  })
   @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
   public ResponseEntity<ResultVo> delete(String idx) {
     ResultVo result = ResultVo.builder().resultData(service.defaultDelete(idx)).build();
