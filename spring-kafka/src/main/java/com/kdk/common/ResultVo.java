8c2f67d9af776785384bb2fcc77857f4c5c7da39
KimDongki <kdkdongki1997@gmail.com>
2021-07-16 09:23:55 +0900

    [feat] Kafka Repo re .. upload

---
+++ spring-kafka/src/main/java/com/kdk/common/ResultVo.java
@@ -0,0 +1,13 @@
+package com.kdk.common;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ResultVo {
+
+  private Object resultData;
+}
