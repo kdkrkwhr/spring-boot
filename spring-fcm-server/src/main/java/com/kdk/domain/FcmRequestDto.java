df0df996d08148f38f07c51fe3292d6aac5cafa3
kdkrkwhr <kdkdongki1997@gmail.com>
2022-01-09 13:23:20 +0900

    [feat] add fcm-server component

---
+++ spring-fcm-server/src/main/java/com/kdk/domain/FcmRequestDto.java
@@ -0,0 +1,18 @@
+package com.kdk.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class FcmRequestDto {
+
+  private String serviceKey;
+  private String token;
+  private String[] tokens;
+  private String messageType;
+  private String message;
+
+}
---
+++ spring-fcm-server/src/main/java/com/kdk/util/FcmDataUtil.java
@@ -0,0 +1,44 @@
+package com.kdk.util;
+
+import java.io.UnsupportedEncodingException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FcmDataUtil {
+
+  public static String pushMultiDataProcessing(String[] tokens, String messageType, String message)
+      throws JSONException, UnsupportedEncodingException {
+    JSONObject body = new JSONObject();
+    JSONArray array = new JSONArray();
+
+    for (String token : tokens) array.put(token);
+
+    body.put("registration_ids", array);
+    body.put("content_available", true);
+
+    JSONObject notification = new JSONObject();
+    notification.put("messageType", messageType);
+    notification.put("message", new JSONObject(message));
+
+    body.put("data", notification);
+
+    return body.toString();
+  }
+
+  public static String pushOneDataProcessing(String token, String messageType, String message)
+      throws JSONException, UnsupportedEncodingException {
+    JSONObject body = new JSONObject();
+
+    body.put("to", token);
+    body.put("content_available", true);
+
+    JSONObject notification = new JSONObject();
+    notification.put("messageType", messageType);
+    notification.put("message", new JSONObject(message));
+
+    body.put("data", notification);
+
+    return body.toString();
+  }
+}
